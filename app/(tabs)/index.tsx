31af9c39caa371d2d0d546126b5167ea0df6e710
russeltjahjadi <russel.tjahjadi@gmail.com>
2026-06-15 21:51:30 -0500

    Adding in more routing with subscription details

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,42 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link
+        href="/onboarding"
+        className={"mt-4 rounded bg-primary text-white p-4"}
+      >
+        Go to onboarding
+      </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className={"mt-4 rounded bg-primary text-white p-4"}
+      >
+        Go to sign in
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className={"mt-4 rounded bg-primary text-white p-4"}
+      >
+        Go to sign up
+      </Link>
+
+      <Link href={"/subscriptions/spotify"}>Spotify Subscriptions</Link>
+
+      <Link
+        href={{
+          pathname: "/subscriptions/[id]",
+          params: { id: "claude" },
+        }}
+      >
+        {" "}
+        Claude Max Subscriptions{" "}
+      </Link>
+    </View>
+  );
+}
